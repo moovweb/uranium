f8de9a7392097bef1c159f3e82d40f5a46caccfb
Sean Jezewski <sean@moovweb.com>
2011-05-17 12:35:46 -0700

    Cleaned up compile script

--- build/enrich.rb
+++ build/enrich.rb
@@ -9,15 +9,10 @@ require 'mechanize'
 
 if ARGV.size < 2
   puts "Syntax: ruby enrich.rb url_prefix build_config.yaml"
-  return
+  exit
 end
 
 prefix = ARGV[0]
-
-def read(file)
-#  File.open("../#{file}","r") {|f| f.read }
-  File.open("#{file}","r") {|f| f.read }
-end
 
 options = []
 options << ["compilation_level", "WHITESPACE_ONLY"] #"SIMPLE_OPTIMIZATIONS"
@@ -29,22 +24,15 @@ options << ["output_info", "statistics"]
 
 
 build_options = YAML::load(File.open(ARGV[1],"r") {|f| f.read} )
-code = ""
 puts build_options
-#code << read(build_options[:external])
-#code << read(build_options[:global])
 
 options << ["code_url", "#{prefix}#{build_options[:external]}"]
 options << ["code_url", "#{prefix}#{build_options[:global]}"]
-
-# options << ["js_code", "#{code}"]
 options << ["js_code", " "]
 
 build_options[:widgets].each do |widget|
-  #code << read(widget)
   options << ["code_url", "#{prefix}#{widget}"]
 end
-
 
 m = Mechanize.new
 result = m.post("http://closure-compiler.appspot.com/compile", options, { "Content-type" => "application/x-www-form-urlencoded" })
