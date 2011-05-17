779b3014f387f59b12c6cb172512abf38c9bfbb8
Sean Jezewski <sean@moovweb.com>
2011-05-17 14:29:10 -0700

    Basic rake file in place. I switched to the (java) app version of GCC because of the size limits

--- build/enrich.rb
+++ build/enrich.rb
@@ -1,42 +1,38 @@
 # Script to build uranium into a single file
+#############################################
+# TODO - To support the blackberry/IE xui versions, I can just add different yaml files
+# 
+
 require 'yaml'
-require 'mechanize'
 
-# TODO
-# - This should eventually have options for webkit/blackberry/ie versions as well
-
-# http://dl.dropbox.com/u/3940085/moovweb/Core/widgets
-
-if ARGV.size < 2
-  puts "Syntax: ruby enrich.rb url_prefix build_config.yaml"
+if ARGV.size == 0
+  puts "Syntax: ruby enrich.rb  build_config_name"
   exit
 end
 
 prefix = ARGV[0]
 
 options = []
-options << ["compilation_level", "WHITESPACE_ONLY"] #"SIMPLE_OPTIMIZATIONS"
-options << ["output_format", "json"]
-options << ["output_info", "compiled_code"]
-options << ["output_info", "warnings"]
-options << ["output_info", "errors"]
-options << ["output_info", "statistics"]
+options << ["compilation_level", "SIMPLE_OPTIMIZATIONS"] #"SIMPLE_OPTIMIZATIONS"
+options << ["js_output_file", "uranium.js"]
 
+build_options = YAML::load(File.open(ARGV[0],"r") {|f| f.read} )
 
-build_options = YAML::load(File.open(ARGV[1],"r") {|f| f.read} )
-puts build_options
+files = [build_options[:external],
+         build_options[:global],
+         build_options[:widgets]].flatten 
 
-options << ["code_url", "#{prefix}#{build_options[:external]}"]
-options << ["code_url", "#{prefix}#{build_options[:global]}"]
-options << ["js_code", " "]
+puts "Compiling:"
+puts "-"*10   
+puts files
+puts "-"*10   
 
-build_options[:widgets].each do |widget|
-  options << ["code_url", "#{prefix}#{widget}"]
+files.each do |widget|
+  options << ["js", File.join("../",widget)]
 end
 
-m = Mechanize.new
-result = m.post("http://closure-compiler.appspot.com/compile", options, { "Content-type" => "application/x-www-form-urlencoded" })
+options.collect! do |option|
+  "--#{option[0]} #{option[1]}"
+end
 
-puts result.code
-
-File.open("uranium.js","w") {|f| f << result.body }
+%x`java -jar ./compiler/compiler.jar #{options.join(" ")}`
