e12920e5678206e05c4ea1e411970d50e730337e
Kevin Liou <kevin.liou@moovweb.com>
2014-03-16 07:03:05 -0700

    tweak spaces

--- src/shInit.js
+++ src/shInit.js
@@ -2,6 +2,7 @@ $(function() {
   var d = SyntaxHighlighter.defaults;
   d["quick-code"] = d["toolbar"] = false;
   $("pre").each(function(_, c) {
+    c.innerHTML = c.innerHTML.replace(/\u2003/g, " "); // get rid of &emsp;
     SyntaxHighlighter.highlight({ brush: $(c).attr("data-lang"), gutter: !$(c).is("[data-no-gutter]") }, c);
   });
   $(document).on("dblclick", ".syntaxhighlighter .container", function() {
