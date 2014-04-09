00a8e1d0b930b104df41a508d3d3ee49fcc688a8
Tevfik Sertel <tsertel@gmail.com>
2014-04-08 19:20:19 -0500

    adding collapse / expand capability to toggler

--- build/src/uranium-pretty.js
+++ build/src/uranium-pretty.js
@@ -169,24 +169,37 @@ var interactions = {};
 
 // Toggler
 interactions.toggler = function( fragment ) {
+  function getRealHeight(item) {
+    var clone;
+    clone = $(item).clone().css({"height":"auto","position":"absolute", "top":"-3000px", "left":"-3000px"}).appendTo("body");
+    var height = clone.height();
+    clone.remove();
+    return height;
+  }
   if (fragment.constructor == Object)
     var groups = assignElements(fragment, "toggler");
   else
     var groups = findElements(fragment, "toggler");
-
   $.each(groups, function(id, group) {
     if (!group["button"])
       $.error("no button found for toggler with id: " + id);
     if (!group["content"])
       $.error("no content found for toggler with id: " + id);
-
+    
     var togglerState = $(group["button"]).attr("data-ur-state") || "disabled";
     $(group["button"]).add(group["content"]).attr("data-ur-state", togglerState);
 
     $(group["button"]).on("click.ur.toggler", function(event) {
       var enabled = $(group["button"]).attr("data-ur-state") == "enabled";
       var newState = enabled ? "disabled" : "enabled";
+      var collapsible = $(group["content"]).attr("data-ur-collapsible") && $(group["content"]).attr("data-ur-collapsible") == "enabled";
       $(group["button"]).add(group["content"]).attr("data-ur-state", newState);
+      // $(group["content"])
+      if (collapsible) {
+        var height = enabled ? "0" : getRealHeight($(group["content"]));
+        // var height = enabled ? "0" : $(group["content"])[0].scrollHeight;
+        $(group["content"]).css("height", height);
+      }
       if (!enabled)
         $(group["drawer"]).attr("data-ur-state", newState);
     });
